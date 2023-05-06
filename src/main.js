f8f8891ccc36aba0622ec14e59f13d7cfeaf3829
Sara Gisondi <sara.gisondi@outlook.it>
2023-05-06 13:30:37 +0200

    modify-searchbar

--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
 import { faBell } from '@fortawesome/free-regular-svg-icons';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faMagnifyingGlass, faBell)
+library.add(faMagnifyingGlass, faBell, faCaretDown)
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
