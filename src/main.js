4a2915a15824da7e97ec8cbd99ac715391d0fb3c
Sara Gisondi <sara.gisondi@outlook.it>
2023-05-07 17:41:41 +0200

    modify input text

--- src/main.js
+++ src/main.js
@@ -6,8 +6,9 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
 import { faBell } from '@fortawesome/free-regular-svg-icons';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import { faCaretUp } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faMagnifyingGlass, faBell, faCaretDown)
+library.add(faMagnifyingGlass, faBell, faCaretDown, faCaretUp)
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
