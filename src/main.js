c265d9a0ef98ac329a426efcdd579d59c2f5bd86
Sara Gisondi <sara.gisondi@outlook.it>
2023-05-07 19:39:50 +0200

    add dropdown

--- src/main.js
+++ src/main.js
@@ -7,8 +7,13 @@ import { faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
 import { faBell } from '@fortawesome/free-regular-svg-icons';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { faCaretUp } from '@fortawesome/free-solid-svg-icons';
+import { faPencil } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRightArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faQuestion } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faMagnifyingGlass, faBell, faCaretDown, faCaretUp)
+
+library.add(faMagnifyingGlass, faBell, faCaretDown, faCaretUp, faPencil, faArrowRightArrowLeft, faUser, faQuestion )
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
