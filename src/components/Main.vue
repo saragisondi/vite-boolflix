044105a6c6e22d0534b492bb7799c3c4b57f9812
Sara Gisondi <sara.gisondi@outlook.it>
2023-05-08 01:05:16 +0200

    modify container-cards

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -19,43 +19,47 @@ export default {
 </script>
 
 <template>
+<div class="container">
 
-<div class="container-cards">
-  
-  <!-- card film-->
-    <Card
-    v-for="film in store.FilmRequest"
-    :key="film"
-    :title="film.title"
-    :originalTitle="film.original_title"
-    :language="film.original_language"
-    :vote="film.vote_average"
-    :img="film.poster_path"
-    :overview="film.overview"
-    />
-  
-    <!-- card series-->
-    <Card
-    v-for="series in store.SeriesRequest"
-    :key="series"
-    :title="series.name"
-    :originalTitle="series.original_name"
-    :language="series.original_language"
-    :vote="series.vote_average"
-    />
+  <div class="container-cards">
+    
+    <!-- card film-->
+      <Card
+      v-for="film in store.FilmRequest"
+      :key="film"
+      :title="film.title"
+      :originalTitle="film.original_title"
+      :language="film.original_language"
+      :vote="film.vote_average"
+      :img="film.poster_path"
+      :overview="film.overview"
+      />
+    
+      <!-- card series-->
+      <Card
+      v-for="series in store.SeriesRequest"
+      :key="series"
+      :title="series.name"
+      :originalTitle="series.original_name"
+      :language="series.original_language"
+      :vote="series.vote_average"
+      />
+  </div>
 </div>
 </template>
 
 <style lang="scss" scoped>
   @use './scss/general.scss' as *;
 
+  .container{
+    margin-top:400px;
+  }
   .container-cards{
     display:flex;
     justify-content: center;
     align-items: center;
     flex-wrap: wrap;
     margin-top: 100px;
-
   }
 
 
